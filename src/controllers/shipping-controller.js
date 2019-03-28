2eb8db3141700a641a589988491cf307588d3840
Julia Mende <julia.gebhardt@de.ibm.com>
2019-03-28 15:50:22 +0100

    revert

--- src/controllers/shipping-controller.js
+++ src/controllers/shipping-controller.js
@@ -2,7 +2,7 @@ var productService = require("../services/product-service");
 
 class ShippingController {
   constructor() {
-    this.REGULAR_PRICE = 0.2;
+    this.REGULAR_PRICE = 0.1;
     this.OVERNIGHT_PRICE = 1;
   }
 
